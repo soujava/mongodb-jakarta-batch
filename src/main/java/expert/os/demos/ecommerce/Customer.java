9519e37a2f7e6db7abbdc2a7853e035ee899289b
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-13 07:47:51 +0100

    feat(ecommerce): enhance Customer entity with constructor, getters, and utility methods

--- src/main/java/expert/os/demos/ecommerce/Customer.java
+++ src/main/java/expert/os/demos/ecommerce/Customer.java
@@ -5,6 +5,7 @@ import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Customer {
@@ -20,4 +21,54 @@ public class Customer {
 
     @Column
     private CustomerTier tier;
+
+    Customer(String id, String name, BigDecimal totalSpent, CustomerTier tier) {
+        this.id = id;
+        this.name = name;
+        this.totalSpent = totalSpent;
+        this.tier = tier;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getTotalSpent() {
+        return totalSpent;
+    }
+
+    public CustomerTier getTier() {
+        return tier;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Customer customer)) {
+            return false;
+        }
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", totalSpent=" + totalSpent +
+                ", tier=" + tier +
+                '}';
+    }
+
+    public static CustomerBuilder builder() {
+        return new CustomerBuilder();
+    }
 }
